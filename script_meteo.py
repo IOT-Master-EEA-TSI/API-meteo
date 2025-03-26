8ea8fdc94fc54aaed5ae1c461388995b58b72905
Goupil117 <84926681+Goupil117@users.noreply.github.com>
2025-03-27 00:10:31 +0100

    Add files via upload

--- script_meteo.py
+++ script_meteo.py
@@ -1,27 +1,43 @@
 import requests
 import argparse
+import json
 
-url = "http://api.weatherapi.com/v1/forecast.json" # Lien internet pour contacter l'API de OpenWeather
-cle = "2c6da332f6aaccb45ccbcaeb7f9b773b" # Clé API
-ville = "Dijon"
+url = "https://api.openweathermap.org/data/2.5/weather" # URL de l'API OpenWeatherMap pour obtenir la météo
+cle = "dea2acb30978b316029d8305f83d07cf" # Clé API pour authentifier la requête
+ville = "Dijon" 
 
+# Création d'un dictionnaire contenant les paramètres de la requête à envoyer 
 parametre = {
-    "key": cle,
-    "q":ville,
-    "lang": "fr"
+    "q" : ville, 
+    "appid" : cle,
+    "units" : "metric",
+    "lang" : "fr"
 }
 
-reponse = requests.get(url, parametre)
+try :
+    reponse = requests.get(url, parametre) # Création et envoie d'une requête http avec les paramètres, et récuprère la réponse
+    reponse.raise_for_status() # Lève une exception automatique si la réponse HTTP est une erreur
 
-if reponse.headers["Content-Type"] == "application/json":
-    data = reponse.json()
+    donnee = reponse.json() # Transforme le contenu JSON reçu en dictionnaire Python
 
-    for day in data["forecast"]["forecastday"]:
-        date = day["date"]
-        condition = day["day"]["condition"]["text"]
-        temp = day["day"]["avgtemp_c"]
-        print(f"{date} : {condition}, {temp}°C")
+    if(donnee.get("cod")) != 200 : # Vérifie si l'API renvoie une erreur dans le contenu
+        raise ValueError(f"erreur API : {donnee.get('message', 'Erreur inconnue')}") # Déclenche une erreur volontaire de type ValueError et récupère le message d’erreur fourni par l’API, ou un défaut
 
-else:
-    print("La réponse n’est pas du JSON :")
-    print(reponse.text)
+    # Extraction des données météo utiles
+    meteo = donnee['weather'][0]['description'] # Description météo
+    temperature = donnee['main']['temp'] # Température actuelle
+    humidite = donnee['main']['humidity'] # Taux d'humidité en %
+    vent = donnee['wind']['speed'] # Vitesse du vent en m/s
+
+    # Affichage des résultats
+    print(f"météo = {meteo}")
+    print(f"temperature = {temperature}°C")
+    print(f"Humidité = {humidite}%")
+    print(f"vent = {vent} m/s")
+
+# Gestion des différentes erreurs possibles :
+except requests.exceptions.RequestException as erreur: # Problème de connexion ou réponse HTTP invalide
+    print("Erreur de connexion ou de requête HTTP :", erreur)
+except ValueError as erreur: # Erreur retournée par l'API dans le contenu JSON
+    print("Erreur de contenu JSON ou API :", erreur)
+
